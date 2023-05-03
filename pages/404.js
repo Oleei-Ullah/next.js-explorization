a6950fc20fa938bac271f3977032b353a3777aa2
ullah <sheikholeei97@gmail.ocm>
2023-05-03 11:20:36 +0600

    redirection automatically.

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,20 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const ErrorPage = () => {
     const router = useRouter();
+    const [redirect, setRedirect] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setRedirect(true);
+            setTimeout(() => {
+                setRedirect(false);
+                router.push('/');
+            }, 3000);
+        }, 3000);
+    }, [router])
     return (
         <div className="flex items-center justify-center h-screen w-full bg-white text-black">
             <div className="relative w-full text-center">
@@ -12,7 +24,9 @@ const ErrorPage = () => {
                     <p className="text-[14px] uppercase text-gray-700 w-[50%] mx-auto">The page you are looking for might have removed or name changed or it is temporarily unavailabe.</p>
                     <Link className="border-2 inline-block bg-blue-500 px-10 py-3 mt-8 text-white text-md rounded-full" href='/'>Back To Homepage</Link>
 
+
                     <button onClick={() => router.push('/')} className="border-2 inline-block bg-blue-500 px-10 py-3 mt-8 text-white text-md rounded-full">Click to Homepage</button>
+                    {redirect && <p>Redirecting to Homepage....</p>}
                 </div>
             </div>
         </div>
