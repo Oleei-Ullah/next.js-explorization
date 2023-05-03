87dd085099113f68e07b52566693e06faf5e6f7f
ullah <sheikholeei97@gmail.ocm>
2023-05-03 21:05:57 +0600

    modification blog

--- pages/blog/[pageno].js
+++ pages/blog/[pageno].js
@@ -34,7 +34,7 @@ const Mydata = ({data}) => {
     <>
     <Navbar />
     <div className="w-[50%] mx-auto shadow-md bg-white mt-10 rounded px-3 py-5">
-        <h1 className="bg-blue-700 rounded-full text-white font-bold text-md inline p-1">{id}</h1>
+        <h1 className="bg-blue-700 rounded-full text-white font-bold text-md inline-block p-1 w-[30px] h-[30px] text-center">{id}</h1>
 
         <h2 className="text-[18px] mt-2 font-semibold text-black">{title}</h2>
         <h3 className="text-[14px] text-black p-1">{body}</h3>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -18,10 +18,10 @@ const Blog = ({ data }) => {
             <div className="w-1/2 mx-auto space-y-4">
                 <h1 className="text-center text-blue-600 font-bold text-[26px]">Blog data</h1>
                 {
-                    data && data.map(({ id, title }) => {
+                    data && data.slice(0,5).map(({ id, title }) => {
                         return (
-                            <div key={id} className="p-2 shadow-md mt-3 flex gap-3">
-                                <h1 className="bg-blue-700 rounded-full text-white font-bold text-md inline p-1">{id}</h1>
+                            <div key={id} className="p-2 shadow-md mt-3 flex items-center gap-3">
+                                <h1 className="bg-blue-700 rounded-full text-white font-bold text-md inline-block p-1 w-[30px] h-[30px] text-center">{id}</h1>
                                 <Link href={`/blog/${id}`}>
                                     <h2 className="capitalize">{title}</h2>
                                 </Link>
